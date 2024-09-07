b8ceeb3b43c76dcb19ff25c2f7e66711f3eca1af
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-07 21:21:50 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local GameName = "slap farm gui - by nexer"
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "slap farm gui! v3.0", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "slap farm gui! v4.0", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
